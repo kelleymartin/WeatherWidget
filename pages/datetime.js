285274f6a9956e797cfd1726681a1c30e60603d3
Kelley Martin <kelleymartin@Kelleys-MBP.attlocal.net>
2020-06-28 22:51:24 -0700

    100s

--- pages/datetime.js
+++ pages/datetime.js
@@ -30,13 +30,13 @@ export default () => {
       .time {
         color: #333;
         font-size: 20px;
-        font-weight: normal;
+        font-weight: 400;
         text-align: center;
       }
       h4 { 
         color: #bd8163;
         font-size: 20px;
-        font-weight: medium;
+        font-weight: 500;
         text-align: center;
         margin: 0px;
       }`}
